b0879d4eebe724bf81e05b408c1d778f21a3f2ec
RiskaMellyAgustin <160058646+RiskaMellyAgustin@users.noreply.github.com>
2025-07-21 02:31:19 +0700

    Update urls.py

--- backend/yieldapp/urls.py
+++ backend/yieldapp/urls.py
@@ -1,10 +1,1 @@
-# backend/yieldapp/urls.py
 
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('upload/', views.upload_csv, name='upload_csv'),
-    path('dashboard/', views.dashboard_operator, name='dashboard_operator'),
-    path('mstd/dashboard/', views.dashboard_mstd, name='dashboard_mstd'),
-]
