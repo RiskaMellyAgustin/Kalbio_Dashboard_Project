ae56570512eb0a3d7e1d6a479db96f9c1735fb2a
RiskaMellyAgustin <160058646+RiskaMellyAgustin@users.noreply.github.com>
2025-07-21 02:31:46 +0700

    Update forms.py

--- backend/yieldapp/forms.py
+++ backend/yieldapp/forms.py
@@ -1,13 +1,1 @@
-# yieldapp/forms.py
-from django import forms
 
-class UploadCSVForm(forms.Form):
-    process_type = forms.ChoiceField(choices=[
-        ('filling', 'Filling'),
-        ('inspection', 'Inspection'),
-        ('assembly', 'Assembly'),
-        ('blistering', 'Blistering'),
-        ('packaging', 'Packaging'),
-        ('handover', 'Handover to Warehouse'),
-    ])
-    file = forms.FileField()
